b614bdc888736e66e4416637edbc255218097b7b
Hǎiliàng Wáng <w@h12.me>
2016-02-17 03:43:19 +0800

    no need to use custom encoding for msg pack

--- msgpack.go
+++ msgpack.go
@@ -1,13 +1,1 @@
 package uuid
-
-import (
-	"gopkg.in/vmihailenco/msgpack.v2"
-)
-
-var (
-	MsgPackID int8 = 'u'
-)
-
-func init() {
-	msgpack.RegisterExt(MsgPackID, UUID{})
-}
--- msgpack_test.go
+++ msgpack_test.go
@@ -1,8 +1,11 @@
 package uuid
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 	"time"
 
@@ -14,8 +17,6 @@ func TestMsgPack(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	// error case
-	//buf, err := msgpack.Marshal(&u)
 	buf, err := msgpack.Marshal(u)
 	if err != nil {
 		t.Fatal(err)
@@ -29,7 +30,7 @@ func TestMsgPack(t *testing.T) {
 	}
 }
 
-func TestFailed(t *testing.T) {
+func TestMsgPackPtr(t *testing.T) {
 	u, err := NewTime(time.Now())
 	if err != nil {
 		t.Fatal(err)
@@ -51,6 +52,37 @@ type S struct {
 	ID UUID
 }
 
+type HexString []byte
+
+func (s HexString) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + hex.EncodeToString([]byte(s)) + `"`), nil
+}
+
+func (s *HexString) UnmarshalJSON(bs []byte) error {
+	bs, err := hex.DecodeString(strings.Trim(string(bs), `"`))
+	if err != nil {
+		return err
+	}
+	*s = HexString(bs)
+	return nil
+}
+
+func RestoreHexString(any interface{}) interface{} {
+	switch o := any.(type) {
+	case map[string]interface{}:
+		for key, value := range o {
+			o[key] = RestoreHexString(value)
+		}
+	case []byte:
+		if len(o) <= size {
+			return HexString(o)
+		}
+	default:
+		fmt.Println(reflect.TypeOf(any))
+	}
+	return any
+}
+
 func TestToJSON(t *testing.T) {
 	var s S
 	s.ID, _ = NewTime(time.Now())
@@ -58,16 +90,17 @@ func TestToJSON(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(s.ID)
 	fmt.Println(len(buf))
+	fmt.Printf("%#v\n", buf)
 	m := make(map[string]interface{})
 	if err := msgpack.Unmarshal(buf, &m); err != nil {
 		t.Fatal(err)
 	}
-	buf, err = json.Marshal(m)
+	buf, err = json.Marshal(RestoreHexString(m))
 	if err != nil {
 		t.Fatal(err)
 	}
+	fmt.Println(len(buf))
 	fmt.Println(string(buf))
 
 }
